3b0301885ad9088eb9e84db9aa68d5931882229d
Kishore Murari <Kishore.Murari@usps.gov>
2021-07-19 19:36:33 +0530

    Removing unused js references

--- bpg/views.py
+++ bpg/views.py
@@ -19,7 +19,8 @@ def init(request):
     if not hasattr(user_data, "userName") or user_data.userName=="" :
         #Return to HomePage without populating links
         return render(request,'bpgtemplate.html')
-    else:            
+    else:
+        #Read XML Services File and generate Links            
         xmldoc = ET.parse(os.path.join(os.path.dirname(__file__),'services.xml'))
         root = xmldoc.getroot()
         
@@ -38,14 +39,11 @@ def init(request):
 
         
 def get_user_name(request):
-    print('Inside get_user_name Function')
     try:
         user_details = UserDetails()
-        auth_url = str(request.build_absolute_uri())+".auth/me"
-        print(auth_url)
         access_token=get_access_token(request)[0]["access_token"] 
         print(access_token)   
-        print ("Calling Graph API")    
+ 
         graph_response = call_graph (access_token)
         print ("Graph API Returned")
         if "givenName" in graph_response:
@@ -66,33 +64,19 @@ def get_access_token(request):
 
     auth_url = str(request.build_absolute_uri())+".auth/me"
     #auth_url = "https://usps-bpg-dev.azurewebsites.net/.auth/me" #Enable for localhost
-    print("URL TO CALL: "+auth_url)
     
     try:
         cookie = request.COOKIES.get("AppServiceAuthSession")
         if cookie is not None:
-            print("Calling Cookie API")
             curSession = requests.Session() # all cookies received will be stored in the session object  
-            response = curSession.get(auth_url,cookies=request.COOKIES)  
-            print ("Status is "+ str(response.status_code))        
-        
-            auth_json = response.json()
-            '''
-        if response.ok:        
-            print(auth_json)
-        else:    
-        
-            if "error" in auth_json:
-                print(auth_json["error"]["code"])        
-            print (response.status_code)
-            '''
+            response = curSession.get(auth_url,cookies=request.COOKIES)                
+            auth_json = response.json()          
         return auth_json
     except Exception as e:
         print ("Inside get_access_token exception")
         print (e)
 
 def call_graph(access_token):
-    print("Inside call_graph API")
     print(access_token)
     response = requests.get("https://graph.microsoft.com/v1.0/me",headers={'Authorization': 'Bearer '+ access_token})
     graph_json = response.json()

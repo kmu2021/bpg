0e017ec16f3b6c3a58eab0f095f04be88241140f
Kishore Murari <Kishore.Murari@usps.gov>
2021-08-20 17:03:59 +0530

    Wnoise

--- bpg_project/settings.py
+++ bpg_project/settings.py
@@ -36,9 +36,9 @@ INSTALLED_APPS = [
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-    'django.contrib.messages',    
-    #'django.contrib.staticfiles',    
-    'whitenoise.runserver_nostatic',
+    'django.contrib.messages',   
+    'whitenoise.runserver_nostatic', 
+    'django.contrib.staticfiles',        
     'bpg',
 ]
 
@@ -124,7 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 #Adding Whitenoise Compression to handle static files
-STATICFILES_STORAGE = ('whitenoise.storage.CompressedManifestStaticFilesStorage')
+STATICFILES_STORAGE = ('whitenoise.storage.CompressedStaticFilesStorage')
 
 STATIC_URL = os.environ.get('DJANGO_STATIC_URL',"/static/")
 STATICFILES_DIRS = [os.path.join(BASE_DIR,'bpg/static')]

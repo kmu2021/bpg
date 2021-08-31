b1572718c3b9766b6d66cc81bf7ccf5869e1f846
Kishore Murari <Kishore.Murari@usps.gov>
2021-08-31 15:00:05 +0530

    Logout

--- bpg/static/scripts/bpg.js
+++ bpg/static/scripts/bpg.js
@@ -1,9 +1,27 @@
 if("undefined"==typeof jQuery)throw new Error("Bootstrap's JavaScript requires jQuery");
 function logout(){		
-	window.alert("sometextan");
-	$.get("https://ile-tm-dev.usps.com/tm/admin/LoginViewController.jsp?ControllerAction=Logout", function(data, status){
-		alert("Data: " + data + "\nStatus: " + status);
-	  });
+	window.alert("Calling FA - Logout");
+	  $.ajax({
+		url: "https://ile-tm-dev.usps.com/tm/admin/LoginViewController.jsp?ControllerAction=Logout",
+		type: "GET",
+		success: (data, status, jqXHR) {
+			window.alert(jqXHR.status);
+		},
+		error: function (error) {
+		window.alert(error);
+		}
+		})
+		window.alert("Calling BA - Logout");
+		$.ajax({
+			url: "https://ile-ba-dev.usps.com/ibmcognos/bi/v1/disp?b_action=xts.run&m=portal/logoff.xts&h_CAM_action=logoff",
+			type: "GET",
+			success: (data, status, jqXHR) {
+				window.alert(jqXHR.status);
+			},
+			error: function (error) {
+			window.alert(error);
+			}
+			})
 	//location.href = 'logout';
 
 }

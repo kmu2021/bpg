4f7927b9107716b5ede6ec6ef4f478b3c2cbfcab
Kishore Murari <Kishore.Murari@usps.gov>
2021-08-31 16:37:21 +0530

    Logout

--- bpg/static/scripts/bpg.js
+++ bpg/static/scripts/bpg.js
@@ -3,12 +3,12 @@ if ("undefined" == typeof jQuery) throw new Error("Missing jQuery");
 function logout() {
     console.log("Initiating Logout");
     for (i = 0; i < logoutUrls.length; i++) {
-        console.log("Calling URL: " + logoutUrls[i]);
+        console.log("Calling URL[" + i + "]: " + logoutUrls[i]);
         $.ajax({
             url: logoutUrls[i],
             type: "GET",
             success: function(data, status, jqXHR) {
-                console.log("Status: " + jqXHR.status);
+                console.log("Status[" + i + "]: " +  jqXHR.status);
             },
             error: function(error) {
                 window.alert(error);

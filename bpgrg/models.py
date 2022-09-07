a2cb016ea7f626884e444e88886303e607991a25
Kishore Murari <Kishore.Murari@usps.gov>
2022-09-08 01:14:13 +0530

    Added Butons

--- bpgrg/models.py
+++ bpgrg/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django import forms
 
 # Create your models here.
+
+# creating a form
+
+
+class RegistrationForm(forms.Form):
+    firstName = forms.CharField(label='First Name', max_length=100, widget=forms.TextInput(
+        attrs={'class': 'form-control'}))
+    lastName = forms.CharField(label='Last Name', max_length=100, widget=forms.TextInput(
+        attrs={'class': 'form-control'}))
+    email = forms.CharField(label='Email', max_length=100, widget=forms.TextInput(
+        attrs={'class': 'form-control'}))
+    company = forms.CharField(label='Company', max_length=100, widget=forms.TextInput(
+        attrs={'class': 'form-control'}))
+    supplierId = forms.IntegerField(
+        label='Supplier ID', widget=forms.TextInput(attrs={'class': 'form-control'}))
--- bpgrg/views.py
+++ bpgrg/views.py
@@ -10,13 +10,55 @@ import os
 from pathlib import Path
 import json
 import requests
+from django.shortcuts import render
+from .models import RegistrationForm
+from django.forms import formset_factory
 
 # Logout Function
+
+
 def logout(request):
     # Redirect to the logout endpoint of Azure Web
     print("Logout Initiated")
-    return HttpResponseRedirect("/.auth/logout")    
+    return HttpResponseRedirect("/.auth/logout")
 
 # Main Init Function
-def init(request):  
-    return render(request, 'bpgrgtemplate.html')
+
+
+def init(request):
+    #    return render(request, 'bpgrgtemplate.html')
+    context = {}
+   # context['form']= RegistrationForm()
+    # creating a formset
+    if request.method == 'GET':
+        RegistrationFormSet = formset_factory(RegistrationForm, extra=1)
+        formset = RegistrationFormSet()
+        context['formset'] = formset
+        return render(request, "bpgrgtemplate.html", context)
+    elif request.method == 'POST':
+        print(request)
+        RegistrationFormSet = formset_factory(RegistrationForm)
+        formset = RegistrationFormSet(data=request.POST)
+        context['formset'] = formset
+        for form in formset:
+            if form.is_valid():
+                        # person = form.save(commit=False)
+                print(form.cleaned_data['lastName'])
+
+        if 'additems' in request.POST and request.POST['additems'] == 'true':
+            print("ADDDING")
+            formset_dictionary_copy = request.POST.copy()
+            formset_dictionary_copy['form-TOTAL_FORMS'] = int(formset_dictionary_copy['form-TOTAL_FORMS']) + 1
+            formset = RegistrationFormSet(formset_dictionary_copy)
+            context['formset'] = formset
+        if 'removeitems' in request.POST and request.POST['removeitems'] == 'true':
+            print("Removing")
+            formset_dictionary_copy = request.POST.copy()
+            formset_dictionary_copy['form-TOTAL_FORMS'] = int(formset_dictionary_copy['form-TOTAL_FORMS']) - 1
+            formset = RegistrationFormSet(formset_dictionary_copy)
+            context['formset'] = formset
+        return render(request, "bpgrgtemplate.html", context)
+    else:
+        return render(request, "bpgrgtemplate.html")
+
+    # Add the formset to context dictionary
